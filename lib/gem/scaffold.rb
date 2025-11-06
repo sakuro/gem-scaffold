0a65f03244a78406b5ac2186247c54de960d1a8a
OZAWA Sakuro <10973+sakuro@users.noreply.github.com>
2025-11-06 10:45:50 +0900

    :computer: bundle gem gem-scaffold

---
+++ lib/gem/scaffold.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "scaffold/version"
+
+module Gem
+  module Scaffold
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
